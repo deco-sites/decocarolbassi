148c55adb003176a9505a6eac345a872f1d01df6
luiz felipe <luizfelipebragaa@gmail.com>
2024-07-04 14:56:51 -0300

    feat: mobile page title

--- components/search/SearchTitle.tsx
+++ components/search/SearchTitle.tsx
@@ -1,7 +1,7 @@
 export default function SearchTitle({ title }: { title: string }) {
   const pageTitle = title.replaceAll("-", " ") ?? title;
   return (
-    <h2 class="ml-[12px] mt-[21px] md:mt-0 md:ml-0 text-[16px] font-light md:font-normal md:text-2xl text-dark-blue md:mb-6 uppercase">
+    <h2 class="ml-[12px] mt-10 md:mt-0 md:ml-0 text-[16px] font-light md:font-normal md:text-2xl text-dark-blue md:mb-6 uppercase">
       {pageTitle}
     </h2>
   );
