2a373c6df412d89b5f1aefc419d19f048ff719e9
luiz felipe <luizfelipebragaa@gmail.com>
2024-07-04 11:23:23 -0300

    feat: newsletter success message

--- components/footer/Newsletter.tsx
+++ components/footer/Newsletter.tsx
@@ -2,6 +2,7 @@ import { useSignal } from "@preact/signals";
 import { ImageWidget } from "apps/admin/widgets.ts";
 import { Picture, Source } from "apps/website/components/Picture.tsx";
 import type { JSX } from "preact";
+import { useRef } from "preact/hooks";
 import { invoke } from "../../runtime.ts";
 import { clx } from "../../sdk/clx.ts";
 
@@ -54,6 +55,7 @@ function Newsletter(
   const { tiled = false } = layout;
   const loading = useSignal(false);
   const successEmailMessage = useSignal("");
+  const emailRef = useRef<HTMLInputElement>(null);
 
   const handleSubmit: JSX.GenericEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
@@ -73,7 +75,10 @@ function Newsletter(
     } finally {
       loading.value = false;
       setTimeout(
-        () => successEmailMessage.value = "",
+        () => {
+          successEmailMessage.value = "";
+          if (emailRef.current) emailRef.current.value = "";
+        },
         5000,
       );
     }
@@ -124,22 +129,24 @@ function Newsletter(
         >
           <div class="flex flex-wrap gap-3 relative w-full">
             <input
+              ref={emailRef}
               name="email"
-              class="flex-auto input input-bordered w-full text-base-content pr-0"
+              class="flex-auto h-10 border-0 outline-none w-full pl-2 pr-0 text-sm text-paragraph-color"
               placeholder={content?.form?.placeholder || "Digite seu email"}
             />
             <button
               type="submit"
-              class="btn absolute right-0 disabled:loading"
+              class="h-10 min-h-10 btn absolute right-0 disabled:loading"
               disabled={loading}
             >
-              <img
-                src="https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/9166/e39a0e1d-b911-4d6c-9fba-0fa6008c6936"
-                alt="Flecha"
-                loading={"lazy"}
-              />
+              <ArrowNewsIcon />
             </button>
             <span
+              style={{
+                color: content?.form?.color !== "#000000"
+                  ? content?.form?.color
+                  : undefined,
+              }}
               class={`absolute -bottom-8 ${
                 successEmailMessage.value ? "block text-neutral-200" : "hidden"
               }`}
@@ -170,3 +177,18 @@ function Newsletter(
 }
 
 export default Newsletter;
+
+const ArrowNewsIcon = () => (
+  <svg
+    width={14}
+    height={12}
+    viewBox="0 0 14 12"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M12.0731 6.50003H0.5C0.357683 6.50003 0.238775 6.45227 0.143275 6.35675C0.0477584 6.26125 0 6.14235 0 6.00003C0 5.85771 0.0477584 5.7388 0.143275 5.6433C0.238775 5.54779 0.357683 5.50003 0.5 5.50003H12.0731L8.33845 1.7654C8.24102 1.66797 8.19006 1.55323 8.18557 1.42118C8.18109 1.28913 8.23205 1.16798 8.33845 1.05773C8.44872 0.947462 8.56763 0.891371 8.6952 0.889454C8.82277 0.887521 8.94168 0.941688 9.05193 1.05195L13.4346 5.43465C13.5218 5.52182 13.583 5.61092 13.6183 5.70195C13.6535 5.79297 13.6712 5.89233 13.6712 6.00003C13.6712 6.10773 13.6535 6.20709 13.6183 6.2981C13.583 6.38914 13.5218 6.47824 13.4346 6.5654L9.05193 10.9481C8.95449 11.0455 8.83878 11.0965 8.7048 11.101C8.57083 11.1055 8.44872 11.0526 8.33845 10.9423C8.23205 10.8321 8.17788 10.7141 8.17595 10.5885C8.17403 10.4628 8.2282 10.3449 8.33845 10.2347L12.0731 6.50003Z"
+      fill="#121926"
+    />
+  </svg>
+);
