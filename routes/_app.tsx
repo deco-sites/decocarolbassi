87be1174780480ec0e70cd9d7c3556e5acf89313
Rafael Assad <rafael.roberto@econverse.com.br>
2024-08-27 15:05:14 -0300

    chore: added gtm tag on _app

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -18,6 +18,15 @@ export default defineApp(async (_req, ctx) => {
 
       {/* Include Icons and manifest */}
       <Head>
+        {/* Google Tag Manager */}
+        <script>
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://trckss.carolbassi.com.br/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PNLHPKB')`}
+        </script>
+
         {/* Enable View Transitions API */}
         <meta name="view-transition" content="same-origin" />
 
@@ -33,6 +42,10 @@ export default defineApp(async (_req, ctx) => {
         {/* Web Manifest */}
         <link rel="manifest" href={asset("/site.webmanifest")} />
       </Head>
+
+      {/* <!-- Google Tag Manager (noscript) --> */}
+      <noscript><iframe src="https://trckss.carolbassi.com.br/ns.html?id=GTM-PNLHPKB"
+      height="0" width="0" style="display:none;visibility:hidden;"></iframe></noscript>
 
       {/* Rest of Preact tree */}
       <ctx.Component />
