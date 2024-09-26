421831a14b6e9958dfc5c7eea2a5fbb8d6a31346
Renato Dantas <renatodantas@outlook.com.br>
2024-09-26 16:34:49 -0300

    feat: new button submit

--- islands/FirstBuyModal/FirstBuyModal.tsx
+++ islands/FirstBuyModal/FirstBuyModal.tsx
@@ -31,19 +31,21 @@ const FirstBuyModal = () => {
   const handleSubmit: JSX.GenericEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
 
-    try {
-      const email = (
-        e.currentTarget.elements.namedItem("email") as RadioNodeList
-      )?.value;
+    const email = (e.currentTarget.elements.namedItem("email") as RadioNodeList)
+      ?.value;
 
-      await invoke.vtex.actions.masterdata.createDocument({
-        data: { email },
-        acronym: "NL",
-      });
-      alert("E-mail cadastrado com sucesso!");
-    } finally {
-      closeModal();
+    if (!email.length) {
+      alert("Preencha um e-mail válido!");
+      return;
     }
+
+    await invoke.vtex.actions.masterdata.createDocument({
+      data: { email },
+      acronym: "NL",
+    });
+    alert("E-mail cadastrado com sucesso!");
+
+    closeModal();
   };
 
   return (
@@ -53,13 +55,13 @@ const FirstBuyModal = () => {
         className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-[100] "
         onClick={handleOutsideClick}
       >
-        <div className="bg-[#fff] w-[918px] relative flex justify-between items-center border-solid border-[1px] max-[1024px]:w-[530px] max-[600px]:w-[90vw] max-[400px]:flex-col max-[400px]:gap-4">
+        <div className="bg-[#fff] w-[918px] relative flex justify-between items-center border-solid border-[1px] max-[1024px]:w-[70%] max-[1024px]:min-w-[530px] max-[600px]:w-[90vw] max-[600px]:min-w-[unset] max-[460px]:flex-col max-[400px]:gap-4">
           <img
             src="https://deco-sites-assets.s3.sa-east-1.amazonaws.com/decocarolbassi/759c4ffa-e292-4bf4-abd7-f5f34933294a/firstBuyModalImage.png"
             alt="Banner"
-            className="max-[1024px]:w-[330px] max-[600px]:w-[200px] max-[400px]:w-full"
+            className="max-[1024px]:w-[330px] max-[600px]:w-[200px] max-[460px]:w-full"
           />
-          <div className="flex flex-col items-center px-10 max-[1024px]:px-4">
+          <div className="flex flex-col items-center px-10 max-[1024px]:px-4 max-[460px]:py-[15px]">
             <h2 className="text-5xl mb-[29px] max-[1024px]:text-[20px] max-[600px]:mb-2">
               GARANTA 10% OFF
             </h2>
@@ -68,7 +70,10 @@ const FirstBuyModal = () => {
               newsletter e receba 10% de desconto na sua próxima compra usando o
               voucher <strong>CAROLBASSI10</strong>
             </p>
-            <form onSubmit={handleSubmit}>
+            <form
+              onSubmit={handleSubmit}
+              className="flex flex-col items-center"
+            >
               <input
                 type="email"
                 name="email"
@@ -80,10 +85,16 @@ const FirstBuyModal = () => {
                 placeholder="E-mail"
                 className="border border-[#000] px-6 w-full rounded-[19px] mb-2 h-[55px] placeholder:text-xs max-[1024px]:h-[37px]"
               />
+              <p className="text-sm text-[#000] self-start max-[1024px]:text-[6px] w-full text-center">
+                *Desconto válido apenas em produtos full price
+              </p>
+              <button
+                type="submit"
+                className="bg-primary-900 text-[#fff] mt-3 mb-[18px] rounded-[19px] h-[33px] w-[125px] text-[12px] max-[1024px]:text-[10px] max-[1024px]:w-[109px] max-[1024px]:h-[27px] max-[1024px]:mb-[15px] max-[600px]:mb-[0]"
+              >
+                INSCREVA-SE
+              </button>
             </form>
-            <p className="text-sm text-[#000] self-start max-[1024px]:text-[6px]">
-              *Desconto válido apenas em produtos full price
-            </p>
           </div>
         </div>
       </div>
