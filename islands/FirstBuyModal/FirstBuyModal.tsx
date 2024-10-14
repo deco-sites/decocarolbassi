7d11d9c08ddc69bf23285b70c71ed770544d2d05
Renato Dantas <renatodantas@outlook.com.br>
2024-10-14 14:17:44 -0300

    style: update modal style

--- islands/FirstBuyModal/FirstBuyModal.tsx
+++ islands/FirstBuyModal/FirstBuyModal.tsx
@@ -57,18 +57,18 @@ const FirstBuyModal = () => {
       >
         <div className="bg-[#fff] w-[918px] relative flex justify-between items-center border-solid border-[1px] max-[1024px]:w-[70%] max-[1024px]:min-w-[530px] max-[600px]:w-[90vw] max-[600px]:min-w-[unset] max-[460px]:flex-col max-[400px]:gap-4">
           <img
-            src="https://deco-sites-assets.s3.sa-east-1.amazonaws.com/decocarolbassi/759c4ffa-e292-4bf4-abd7-f5f34933294a/firstBuyModalImage.png"
+            src="https://deco-sites-assets.s3.sa-east-1.amazonaws.com/decocarolbassi/087978e2-9606-47d1-a280-8a68f936ad30/banner_first_buy_modal.png"
             alt="Banner"
             className="max-[1024px]:w-[330px] max-[600px]:w-[200px] max-[460px]:w-full"
           />
           <div className="flex flex-col items-center px-10 max-[1024px]:px-4 max-[460px]:py-[15px]">
-            <h2 className="text-5xl mb-[29px] max-[1024px]:text-[20px] max-[600px]:mb-2">
+            <h2 className="font-[Montserrat] text-[37px] mb-[29px] max-[1024px]:text-[20px] max-[600px]:mb-2">
               GARANTA 10% OFF
             </h2>
-            <p className="text-center mb-6 max-[1024px]:text-[9px] max-[600px]:mb-2">
-              Quer estar sempre por dentro das últimas tendências? Assine nossa
-              newsletter e receba 10% de desconto na sua próxima compra usando o
-              voucher <strong>CAROLBASSI10</strong>
+            <p className="font-[Montserrat] text-[18px] text-center mb-6 max-[1024px]:text-[9px] max-[600px]:mb-2">
+              Quer estar sempre por dentro das nossas novidades? <br></br>Assine
+              nossa newsletter e receba 10% de desconto na sua próxima compra
+              usando o voucher: <strong>CAROLBASSI10</strong>
             </p>
             <form
               onSubmit={handleSubmit}
@@ -85,7 +85,7 @@ const FirstBuyModal = () => {
                 placeholder="E-mail"
                 className="border border-[#000] px-6 w-full rounded-[19px] mb-2 h-[55px] placeholder:text-xs max-[1024px]:h-[37px]"
               />
-              <p className="text-sm text-[#000] self-start max-[1024px]:text-[6px] w-full text-center">
+              <p className="font-[Montserrat] text-sm text-[#000] self-start max-[1024px]:text-[6px] w-full text-center">
                 *Desconto válido apenas em produtos full price
               </p>
               <button
